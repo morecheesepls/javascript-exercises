7ad04d95bc53352b79e464073b3059e21b0c121d
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-02-25 15:15:51 -0800

    Initial Commit
    Started exercise 02_repeatString. However, still trying to figure out how the VSCode debugger works.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
 const repeatString = function() {
-
+    const repeatWord = "hey";
+    
+    for (i = 0; i < 3; i++) {
+        console.log(repeatWord);
+    }
 };
+
+repeatString;
 
 // Do not edit below this line
 module.exports = repeatString;
