5dc04936e20f7b5db5ddc677ad73c3413d32e530
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-02-26 14:33:19 -0800

    Continue Exercise04
    After some research, discovered that rest parameters are not real arrays so I cannot use certain parameters (like .find with them).
    Started from scratch and still use rest parameters, but have converted them into a real array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,10 @@
-let myArray = [1, 2, 3, 4];
-let num;
-
-const removeFromArray = function(myArray, num) {
-    console.log(myArray, num);
+const removeFromArray = function() {
+    console.log(args = [...arguments]); // convert the arguments into a real array
 };
 
-removeFromArray(myArray, num);
+
+
+removeFromArray([1, 2, 3, 4], 3, 5, 7, 9);
 
 // Do not edit below this line
 module.exports = removeFromArray;
