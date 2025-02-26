8464506d05ac51d78aeb0003dc05ffb2a98d470c
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-02-26 10:40:44 -0800

    Exercise 2 Completed
    All jest tests now pass successfully.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,28 @@
-const repeatString = function() {
-    const repeatWord = "hey";
-    
-    for (i = 0; i < 3; i++) {
-        console.log(repeatWord);
+const num = Math.floor(Math.random() * 1000);
+
+function repeatString(word, num) {
+    if (num < 0) {
+        console.log(word = "ERROR");
+        return word;
+    } else if (num === 0) {
+        console.log(word = "");
+        return word;
+    } else if (num > 0) {
+        wordArray = [];
+
+        for (let i = 0; i < num; i++) {
+            wordArray.push(word);
+        }
+
+        for (item of wordArray) {
+            console.log(newArray = wordArray.join(""));
+        }
+        return newArray;
     }
 };
 
-repeatString;
+repeatString("", num);
+
 
 // Do not edit below this line
 module.exports = repeatString;
