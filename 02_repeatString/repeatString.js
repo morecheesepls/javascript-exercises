10a7a5ad9baceb509effb17fc31b18c81ac31702
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-02-26 10:48:25 -0800

    Added Comments
    Created comments on a couple of lines so that I'm able to review what I did for later exercises.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,4 @@
-const num = Math.floor(Math.random() * 1000);
+const num = Math.floor(Math.random() * 1000); // assigns a random number from 0 to 999
 
 function repeatString(word, num) {
     if (num < 0) {
@@ -7,7 +7,7 @@ function repeatString(word, num) {
     } else if (num === 0) {
         console.log(word = "");
         return word;
-    } else if (num > 0) {
+    } else if (num > 0) { // if num is > 0, add word to wordArray, then join all strings in wordArray so that no spaces exist. 
         wordArray = [];
 
         for (let i = 0; i < num; i++) {
