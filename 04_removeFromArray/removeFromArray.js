c0b4bc4ae899ecc29b350f7eb9c27307ee48085f
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-02-26 11:51:51 -0800

    Started Exercise04
    Added a variable that is assigned an array
    Added arguments to the function for the array and a number

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+let myArray = [1, 2, 3, 4];
+let num;
+
+const removeFromArray = function(myArray, num) {
+    console.log(myArray, num);
 };
+
+removeFromArray(myArray, num);
 
 // Do not edit below this line
 module.exports = removeFromArray;
