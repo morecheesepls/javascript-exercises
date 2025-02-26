e96f551396d66adff7dfa84a439895a32471173a
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-02-26 14:59:54 -0800

    Continue Exercise04
    - Separated the rest parameters from the first array parameter.
    - Created a function to remove any of the rest parameters from the array parameter, but it does not work yet.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
 const removeFromArray = function() {
-    console.log(args = [...arguments]); // convert the arguments into a real array
+    args = [...arguments]; // convert the arguments into a real array
+    
+    for (arg of args) {
+        if (arg !== args[0]) {
+            console.log(notArgOne = arg);
+        }
+        continue;
+    }
+
+    for (arg of args[0]) {
+        if (notArgOne === arg) {
+            return console.log(newArray = args[0].slice(arg));
+        }
+    }
 };
 
 
