5b9c3d658bdd44a7128fe95a8aa6c286586d37c9
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-02-26 11:12:48 -0800

    Started Exercise03
    Added function to reverse the string. Jest tests have not been executed.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
 const reverseString = function() {
+    text = "hello there";
+    text = text.split("");
+    text = text.reverse();
+    text = text.join("");
 
+    return text; 
 };
+
+console.log(reverseString());
+
 
 // Do not edit below this line
 module.exports = reverseString;
