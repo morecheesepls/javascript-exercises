205f0ed44968a1c7fb82edbcef8f5e769b3cd1ed
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-02-27 15:23:54 -0800

    Continue Exercise05
    - BUG: The part of the function that loops through the args array and adds the element to the expandedArray variable is looping twice.
        - Theory: This is because on the last step of the 1st loop, it checks the value of i PRIOR to iterating on the value of i.
        - When it starts its 2nd loop, the value of i is still the same as args[1]. So the 2nd loop starts, then i gets iterated.
        - Need to figure out how to iterate i BEFORE it does its check for whether it should loop again.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,26 +1,28 @@
 const sumAll = function() {
     args = [...arguments].sort();
     expandedArray = [];
+    sum = 0;
     
     for (arg of args) {
         if (arg < 0 || typeof arg !== "number" || !Number.isInteger(arg)) {
             return console.log("ERROR");
         } else {
-            for (let i = args[0]; i <= args[1]; i++) {
-                expandedArray.push(i);
+            for (let i = args[0]; i <= args[1];) {
+                expandedArray.push(i++);
             }
-
-            return console.log(expandedArray);
         }
     };
 
+    for (num of expandedArray) {
+        sum += num;
+    }
 
-
+    return console.log(sum);
 };
 
 
 
-sumAll(1, 10);
+sumAll(10, 15);
 
 // Do not edit below this line
 module.exports = sumAll;
--- 05_sumAll/test.js
+++ 05_sumAll/test.js
@@ -6,4 +6,5 @@ for (let i = myArray[0]; i <= myArray[1]; i++) {
     expandedArray.push(i);
 };
 
+
 console.log(expandedArray);
