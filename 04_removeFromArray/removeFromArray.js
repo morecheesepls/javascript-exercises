02d9364767b8d215f3db41df30643c935b81f21a
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-02-27 08:57:19 -0800

    Continue Exercise04
    - Still working getting the functionality working to filter the first argument array based on the content of the other arguments.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,21 +1,26 @@
 const removeFromArray = function() {
     args = [...arguments]; // convert the arguments into a real array
+    argOne = args[0]; // stores only the first argument as a variable
+    argRest = []; // stores all arguments after the first in its own array
     
     for (arg of args) {
         if (arg !== args[0]) {
-            console.log(notArgOne = [arg]);
+            argRest.push(arg);
         }
-        continue;
-    }
+    };
 
-    for (arg of args[0]) {
-        if (notArgOne === arg) {
-            return console.log(newArray = args[0].slice(arg));
-        }
-    }
+    // if (argOne === argRest) {
+    //     console.log("There is nothing to remove.");
+    // } else if (argOne !== argRest) {
+    //     finalArray = argRest.forEach(argOne.filter());
+    // }; 
+
+    // console.log(finalArray);
+
+    argRest.forEach((element) => console.log(argOne.slice(element)));
+
+
 };
-
-
 
 removeFromArray([1, 2, 3, 4], 3, 5, 7, 9);
 
