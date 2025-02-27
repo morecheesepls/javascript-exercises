06dfa958e071ac65aee8da2ff63c0f6016165d4d
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-02-27 12:07:42 -0800

    Start Exercise05
    - Assigned rest parameters to the args array variable, then sort the array from small to high.
    - Created newArray function
        - Loops through the args array and does error checks for negative, non-number and non-integer values.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,5 @@
 const removeFromArray = function() {
     args = [...arguments]; // convert the arguments into a real array
-    argOne = args[0]; // stores only the first argument as a variable
     argRest = []; // stores all arguments after the first in its own array
     finalArray = []; 
     
@@ -10,7 +9,7 @@ const removeFromArray = function() {
         }
     };
 
-    for (num of argOne) {
+    for (num of args[0]) {
         if (!argRest.includes(num)) {
             finalArray.push(num);
         }
