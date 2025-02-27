20c5ac96ed8fb74c0612067f5fce9930a824abba
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-02-27 12:35:33 -0800

    Continue Exercise05
    - bug fix in the function error tracking

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
 const sumAll = function() {
+    args = [...arguments].sort();
+    console.log(args);
+    
+    const newArray = function() {
+        for (arg of args) {
+            if (arg < 0 || typeof arg !== "number" || !Number.isInteger(arg)) {
+                return console.log("ERROR");
+            }
+        }
+    }
+
+    return newArray();
 
 };
+
+sumAll(1, 10);
 
 // Do not edit below this line
 module.exports = sumAll;
