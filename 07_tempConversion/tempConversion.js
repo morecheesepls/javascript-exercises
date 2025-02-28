93d082374b860b773e9a751a74584accb9f33bfd
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-02-28 11:35:19 -0800

    Continue Exercise07
    NOTE: Previous commit may have indicated this was Exercise06 in error.
    - Added logic to display result as a whole number if there is no decimal and if there is a decimal, only display the result to 1 decimal place.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,16 +1,16 @@
 const convertToCelsius = function(f) {
   let f2c;
   f2c = (f-32) * (5/9);
-  return f2c.toFixed(1);
+  return parseFloat(f2c.toFixed(1));
 };
 
 const convertToFahrenheit = function(c) {
   let c2f;
   c2f = ((c*(9/5)) + 32);
-  return c2f.toFixed(1);
+  return parseFloat(c2f.toFixed(1));
 };
 
-console.log(`F to C: ${convertToCelsius(-10)}`);
+console.log(`F to C: ${convertToCelsius(100)}`);
 console.log(`C to F: ${convertToFahrenheit(73.2)}`);
 
 
