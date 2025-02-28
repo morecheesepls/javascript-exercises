f09cae0a5c4869b3fd3336d9a978ffd0968684d4
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-02-28 11:15:30 -0800

    Starting Exercise07
    - Added functions for conversion of fahrenheit to celsius (and vice versa), as well as rounding to 1 decimal place.

---
+++ 07_tempConversion/playground.js
@@ -0,0 +1,9 @@
+let c2f;
+let f2c;
+
+function c2f() {
+    c2f = (c*2)+30
+    return c2f;
+};
+
+console.log(c2f);
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let f2c;
+  f2c = (f-32) * (5/9);
+  return f2c.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let c2f;
+  c2f = ((c*(9/5)) + 32);
+  return c2f.toFixed(1);
 };
+
+console.log(`F to C: ${convertToCelsius(-10)}`);
+console.log(`C to F: ${convertToFahrenheit(73.2)}`);
+
 
 // Do not edit below this line
 module.exports = {
