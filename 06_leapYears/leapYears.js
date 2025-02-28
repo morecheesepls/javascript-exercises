8c2fb0c30321630c222d929549e2543f55774b18
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-02-28 10:26:48 -0800

    Start Exercise06
    - Added logic to check whether a given year is a leap year.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 400 === 0) return true;
+        else if (year % 100 !== 0 && year % 0 === 0) return true;
+        else return false;
 };
 
+
+console.log(leapYears(2100));
 // Do not edit below this line
 module.exports = leapYears;
---
+++ 06_leapYears/playground.js
@@ -0,0 +1,10 @@
+function runners() {
+    return ["Sandra", "Ola", "Chi", "Chucky"];
+  }
+  
+  const [a, b, c, d] = runners();
+  
+  console.log(a); // Output: Sandra
+  console.log(b); // Output: Ola
+  console.log(c); // Output: Chi
+  console.log(d);
