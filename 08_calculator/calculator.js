8361b5950f60094ca3aa61523e01fa10eca0b21d
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-03-31 10:21:01 -0700

    Exercise 08 Calculator Completed
    All 19 tests pass.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,31 @@
-const add = function() {
+const add = function(a, b) {
+	return a+b;
+};
+
+const subtract = function(a, b) {
+	return a-b;
+};
+
+const sum = function(array) {
+  const sumOfArray = array.reduce((total, currentValue) => total + currentValue, 0);
+  return sumOfArray;
+};
+
+const multiply = function(arr) {
+  const productOfArray = arr.reduce((total, currentValue) => total * currentValue);
+  return productOfArray;
+};
+
+const power = function(a, b) {
+	return a ** b;
+};
+
+const factorial = function(num) {
+  if (num === 0 || num === 1) return 1;
+  return num * factorial(num - 1);
+  };
 	
-};
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
