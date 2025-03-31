b4dd4d9c55aa5c8e667eb8e26b16a27fcfc2ac64
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-03-31 11:27:33 -0700

    Exercise 09 palindromes completed
    All 8 tests pass.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+// remove all special characters
+// remove all spaces
+// convert to lowercase
+// reverse the string and store in new variable
+// compare the converted old and new string, return true/false
 
+
+const palindromes = function (string) {
+    let cleanedString = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    let reversedString = cleanedString.split("").reverse().join("").toLowerCase();
+    if (cleanedString == reversedString) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+console.log(palindromes('Animal loots foliated detail of stool lamina.'));
 
 // Do not edit below this line
 module.exports = palindromes;
