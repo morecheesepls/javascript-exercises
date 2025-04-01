19c87041597fa12801f7a1b6e4ab66c4914d2ca7
morecheesepls <11498640+morecheesepls@users.noreply.github.com>
2025-04-01 09:59:17 -0700

    exercise 10 fibonacci completed
    all tests pass

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+// create a loop that loops for the number of times passed to the function (num)
+// loop starts at 1
+// on each pass, add the current number to the previous number
 
+
+
+const fibonacci = function(index) {
+    let array = [1, 1];
+    let result;
+    index = Math.round(index);
+    if (index === 0) {
+        return 0;
+    } else if (index < 0) {
+        return "OOPS";
+    } else {
+        for (let i = 3; i <= index; i++) {
+            result = array[array.length - 2] + array[array.length - 1];
+            array.push(result);
+        };
+    };
+    
+    return array.at(-1);
 };
+
+console.log(fibonacci(-25));
 
 // Do not edit below this line
 module.exports = fibonacci;
